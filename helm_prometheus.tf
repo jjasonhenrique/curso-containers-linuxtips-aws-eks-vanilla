da611ed39d3a74b2801ca148f1331e7a980dde47
Jason Henrique <jhmr.jason@outlook.com>
2025-05-17 12:14:49 -0300

    Aula 29

--- helm_prometheus.tf
+++ helm_prometheus.tf
@@ -14,7 +14,8 @@ resource "helm_release" "prometheus" {
 
   depends_on = [
     aws_eks_cluster.main,
-    aws_eks_node_group.main
+    aws_eks_node_group.main,
+    aws_eks_node_group.prometheus
   ]
 }
 
